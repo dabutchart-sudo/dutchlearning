e7c60ac833dcae4d73ded79ce73d01c866f80844
dabutchart-sudo <dabutchart@googlemail.com>
2026-09-10 14:29:17 +0100

    feat: add flashcard domain model

---
+++ src/engine/flashcards.js
@@ -0,0 +1,7 @@
+export const PRODUCTION_STAGE = Object.freeze({
+  RECOGNITION: 'recognition',
+  SUPPORTED: 'supported-production',
+  GUIDED: 'guided-production',
+  INDEPENDENT: 'independent-production',
+  CONTEXTUAL: 'contextual-production'
+});
